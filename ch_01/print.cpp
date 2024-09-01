70b4a07b40d69152d0413783dd2a70a1f9437448
Abi Liu <abiliu018@gmail.com>
2024-08-31 19:29:02 -0700

    first commit

---
+++ ch_01/print.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+  cout << "I am blaxxon,\n"; 
+  cout << "the godlike computer\n"; 
+  cout << "fear me!!!\n";
+  return 0;                           
+}
